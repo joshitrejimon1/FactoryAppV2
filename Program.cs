849f3bf8f79e1aa3b71cd39626e391178748943c
joshitrejimon1 <joshit.rejimon@aurionpro.com>
2023-09-21 12:19:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryAppV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarFactory BMWFactory = new BMWFactory();
+        }
+    }
+}
